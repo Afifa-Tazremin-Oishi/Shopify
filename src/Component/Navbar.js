31ee60bdfe30bd34c991ad111794f2b531d6f6bc
Afifa-Tazremin-Oishi <afifatazreminoishi@gmail.con>
2022-03-27 01:48:53 +0600

    sixth

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <a className="navbar-brand" href="#"><strong>Online Shop</strong></a>
+          <a className="navbar-brand" href="#"><h6><strong>Online Shop</strong></h6></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
